a8edbaaa9b002df07faa7ad0fcb43039e87ed1c2
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-05-22 09:06:20 -0400

    Refactor to use StepperCar class for movement

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -32,7 +32,7 @@ class HCSR04:
         time_us = time_pulse_us(self.echo, 1)
 
         if time_us <= 0:
-            return -1
+            return -1.0
 
         distance_cm = time_us * 0.5 * 0.0343
 
@@ -86,14 +86,39 @@ class Stepper28BYJ48:
         self.turn_degrees(degrees, delay_ms)
 
 
+class StepperCar:
+    def __init__(
+        self, stepper_1: Stepper28BYJ48, stepper_2: Stepper28BYJ48, track: float, wheel_diameter: float
+    ) -> None:
+        self.stepper_1 = stepper_1
+        self.stepper_2 = stepper_2
+        self.track = track
+        self.wheel_diameter = wheel_diameter
+
+    def step(delay_ms: int = 1) -> None:
+        self.stepper_1.step(1)
+        self.stepper_2.step(-1)
+
+    def move_cm(cm, delay_ms: int = 1) -> None:
+        self.stepper_1.turn_cm(cm)
+        self.stepper_2.turn_cm(-cm)
+
+    def turn_degrees(degrees, delay_ms: int = 1) -> None:
+        target_wheel_degrees = int((TRACK * 90) / WHEEL_DIAMETER)
+
+        self.stepper_1.turn_degrees(-target_wheel_degrees)
+        self.stepper_2.turn_degrees(-target_wheel_degrees)
+
+
 # constants
-TRACK = 100
-WHEEL_DIAMETER = 20
+TRACK = 100.0
+WHEEL_DIAMETER = 20.0
 
 # initialize hardware instances
 STEPPER_1 = Stepper28BYJ48(pin0, pin1, pin2, pin3)
 STEPPER_2 = Stepper28BYJ48(pin12, pin13, pin14, pin15)
 SONAR = HCSR04(trigger=pin9, echo=pin10)
+CAR = StepperCar(STEPPER_1, STEPPER_2, TRACK, WHEEL_DIAMETER)
 
 while True:
     # get distance
@@ -101,14 +126,10 @@ while True:
 
     if distance_cm <= 10:
         # turn wheels backwards 10 cm
-        STEPPER_1.turn_cm(-10)
-        STEPPER_2.turn_cm(-10)
+        CAR.move_cm(-10)
 
         # rotate 90 degrees in place
-        wheel_degrees = int((TRACK * 90) / WHEEL_DIAMETER)
-        STEPPER_1.turn_degrees(wheel_degrees)
-        STEPPER_2.turn_degrees(-wheel_degrees)
+        CAR.turn_degrees(90)
     else:
         # move forward
-        STEPPER_1.step(1)
-        STEPPER_2.step(1)
+        CAR.step()
