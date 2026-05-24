f6b026a8d6b521d8c79ffb9470ff5fe3c35e783d
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-05-23 22:21:20 -0400

    Update main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -9,15 +9,10 @@ from machine import time_pulse_us
 import math
 import utime
 
-# turn off display for more pins
-display.off()
-
 
 PCA9685_ADDR = 0x40
 MODE_1 = 0x00
 MODE_2 = 0x01
-
-
 
 
 class HCSR04:
@@ -25,7 +20,6 @@ class HCSR04:
         self.trigger = trigger
         self.echo = echo
 
-        # initialize pins
         self.trigger.write_digital(0)
         self.echo.read_digital()
 
@@ -47,162 +41,138 @@ class HCSR04:
 
 
 class Driver28BYJ48:
-    HALF_STEPS_PER_REV = 4096
     HALF_STEP_SEQUENCE = [
-        [1, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 1],
-        [0, 0, 0, 1],
-        [1, 0, 0, 1],
-        [1, 0, 0, 0],
+        (1, 0, 0, 0),
+        (1, 1, 0, 0),
+        (0, 1, 0, 0),
+        (0, 1, 1, 0),
+        (0, 0, 1, 0),
+        (0, 0, 1, 1),
+        (0, 0, 0, 1),
+        (1, 0, 0, 1),
     ]
 
-    def __init__(self, index: int) -> None:
-        self.step_index = 0
+    STEPS_PER_REVOLUTION = 4096
+
+    def __init__(self) -> None:
+        self.is_on = [False, False]
+        self.target_time = [0, 0]
+        self.step_index = [0, 0]
+        self.delay = 5000
+        self.init_PCA9685()
 
     def init_PCA9685(self) -> None:
-        i2c.init(sda=pin20, scl=pin19)
-        self.set_frequency(50)
+        i2c.init()
+
+        self.write(0x00, 0x00)
+        prescale = int(25_000_000 / (50 * 4096) - 1)
+
+        self.write(0x00, 0x10)
+        self.write(0xFE, prescale)
+        self.write(0x00, 0x00)
+        self.write(0x00, 0xA1)
 
         for index in range(16):
-            self.set_PWM(index, 0, 0)
+            self.write_PWM(index, 0, 0)
 
-        i2c.write(PCA9685_ADDR, 0x00, 0x00)
+    def write(self, regular: int, value: int) -> None:
+        i2c.write(PCA9685_ADDR, bytes([regular, value]))
 
-
-    def set_frequency(self, frequency: int) -> None:
-        pre_scale_value = 25_000_000 / (frequency * 4096)
-        pre_scale_value -= 1
-
-        old_mode = i2c.read(PCA9685_ADDR, 0x00)
-        new_mode = (old_mode & 0x7f) | 0x10
-
-        i2c.write(PCA9685_ADDR, 0x00, new_mode)
-        i2c.write(PCA9685_ADDR, 0xFE, pre_scale_value)
-        i2c.write(PCA9685_ADDR, 0x00, old_mode)
-        utime.sleep_us(5000)
-        i2c.write(PCA9685_ADDR, 0x00, old_mode | 0xa1)
-
-
-    def set_PWM(self, channel: int, on: int, off: int) -> None:
+    def write_PWM(self, channel: int, on: int, off: int) -> None:
         buffer = bytes([
             0x06 + 4 * channel,
             on & 0xff,
-            (on >> 8) & 0xFF,
+            on >> 8,
             off & 0xFF,
-            (off >> 8) & 0xFF,
+            off >> 8,
         ])
 
         i2c.write(PCA9685_ADDR, buffer)
 
-    def set_stepper(self, index: int, direction: int) -> None:
-        if index == 1:
-            if direction == 1:
-                self.set_PWM(0, 2047, 4095)
-                self.set_PWM(2, 1, 2047)
-                self.set_PWM(1, 1023, 3071)
-                self.set_PWM(3, 3071, 1023)
+    def set_step(self, base: int, step: int) -> None:
+        pattern = self.HALF_STEP_SEQUENCE[step]
+        for index in range(4):
+            if pattern[index]:
+                self.write_PWM(base + index, 0, 4095)
             else:
-                self.set_PWM(3, 2047, 4095)
-                self.set_PWM(1, 1, 2047)
-                self.set_PWM(2, 1023, 3071)
-                self.set_PWM(0, 3071, 1023)
-        else:
-            if direction == 1:
-                self.set_PWM(4, 2047, 4095)
-                self.set_PWM(6, 1, 2047)
-                self.set_PWM(5, 1023, 3071)
-                self.set_PWM(7, 3071, 1023)
-            else:
-                self.set_PWM(7, 2047, 4095)
-                self.set_PWM(5, 1, 2047)
-                self.set_PWM(6, 1023, 3071)
-                self.set_PWM(4, 3071, 1023)
-            
+                self.write_PWM(base + index, 0, 0)
 
-    def stepper_degree(self, index: int, degrees: int) -> None:
-        if degrees > 0:
-            self.set_stepper(index, 1)
-        else:
-            self.set_stepper(index, -1)
+    def step(self, index: int, direction: int) -> None:
+        base = index * 4
+        self.step_index[index] = (
+            self.step_index[index] + direction
+        ) % len(self.HALF_STEP_SEQUENCE)
 
-        degrees = abs(degrees)
-        utime.sleep_ms(degrees * 10240 / 360)
+        self.set_step(base, self.step_index[index])
 
-        self.stop_all_steppers()
+        utime.sleep_us(self.delay)
 
-    def stepper_on(self, index: int, direction: int) -> None:
-        self.set_stepper(index, direction)
+    def step_all(self, directions: list[int]) -> None:
+        for index, direction in enumerate(directions):
+            self.step(index, direction)
 
-    def stop_all_steppers(self) -> None:
-        for index in range(2):
-            self.stepper_off(index)
+    def stop(self, index: int) -> None:
+        base = index * 4
+        for index in range(4):
+            self.write_PWM(base + index, 0, 0)
 
-    def stepper_off(self, index: int) -> None:
-        self.set_PWM(index * 2, 0, 0)
-        self.set_PWM(index * 2 + 1, 0, 0)
+    def stop_all(self) -> None:
+        for index in range(16):
+            self.write_PWM(index, 0, 0)
 
-    def turn_cm(
-        self, distance: int, wheel_diameter: int = 1, delay_ms: int = 1
-    ) -> None:
+    def set_delay(self, delay_us: int) -> None:
+        self.delay = delay_us
 
+    def move_cm(self, distance: int, wheel_diameter: int = 10) -> None:
         circumference = math.pi * wheel_diameter
-        degrees = int((distance * 360) / circumference)
+        revolutions = distance / circumference
+        steps = abs(int(revolutions * self.STEPS_PER_REVOLUTION))
 
-        self.turn_degrees(degrees, delay_ms)
+        for _ in range(steps):
+            if distance > 0:
+                direction = 1
+            else:
+                direction = -1
 
+            self.step_all([direction, direction])
 
-class StepperCar:
-    def __init__(
-        self,
-        stepper_left: Stepper28BYJ48,
-        stepper_right: Stepper28BYJ48,
-        track: float,
-        wheel_diameter: float,
-    ) -> None:
+    def turn_degrees(self, degrees: int, wheel_diameter: int, track: int) -> None:
+        track_circumference = math.pi * track
+        wheel_circumfrence = math.pi * wheel_diameter
 
-        self.stepper_left = stepper_left
-        self.stepper_right = stepper_right
-        self.track = track
-        self.wheel_diameter = wheel_diameter
+        rotations = degrees * track_circumference / wheel_circumfrence / 360
+        steps = abs(int(rotations * self.STEPS_PER_REVOLUTION))
 
-    def step(delay_ms: int = 1) -> None:
-        self.stepper_left.step(1)
-        self.stepper_right.step(-1)
+        for _ in range(steps):
+            if degrees > 0:
+                direction = 1
+            else:
+                direction = -1
 
-    def move_cm(cm, delay_ms: int = 1) -> None:
-        self.stepper_left.turn_cm(cm)
-        self.stepper_right.turn_cm(-cm)
-
-    def turn_degrees(degrees, delay_ms: int = 1) -> None:
-        target_wheel_degrees = int((TRACK * 90) / WHEEL_DIAMETER)
-
-        self.stepper_left.turn_degrees(-target_wheel_degrees)
-        self.stepper_right.turn_degrees(-target_wheel_degrees)
+            self.step_all([direction, -direction])
 
 
 # constants
-TRACK = 100.0
-WHEEL_DIAMETER = 20.0
+TRACK = 20
+WHEEL_DIAMETER = 6
 
 # initialize hardware instances
-STEPPER_1 = Stepper28BYJ48(pin0, pin1, pin2, pin3)
-STEPPER_2 = Stepper28BYJ48(pin12, pin13, pin14, pin15)
-SONAR = HCSR04(trigger=pin9, echo=pin10)
-CAR = StepperCar(STEPPER_1, STEPPER_2, TRACK, WHEEL_DIAMETER)
+STEPPER_DRIVER = Driver28BYJ48()
+SONAR = HCSR04(trigger=pin1, echo=pin2)
 
 while True:
     # get distance
     distance_cm = SONAR.get_distance_cm()
+    print(distance_cm)
 
-    if distance_cm <= 10:
+    if -1 < distance_cm <= 10:
         # turn wheels backwards 10 cm
-        CAR.move_cm(-10)
+        STEPPER_DRIVER.stop_all()
+        STEPPER_DRIVER.move_cm(-10, WHEEL_DIAMETER)
 
         # rotate 90 degrees in place
-        CAR.turn_degrees(90)
+        STEPPER_DRIVER.turn_degrees(90, WHEEL_DIAMETER, TRACK)
     else:
         # move forward
-        CAR.step()
+        STEPPER_DRIVER.step_all([1, 1])
+
